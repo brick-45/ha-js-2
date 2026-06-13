a0596fe50c13d3a6204f49fe80c4c47e86a3852e
brick-45 <polishtshuksoffia@gmail.com>
2026-06-13 17:35:46 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,27 @@
-var fahrenheit = 1432;
-var celcium = 12;
+// Створити змінні celsius, fahrenheit для зберігання температури за Цельсієм та Фаренгейтом. Перевести температуру з Цельсія у Фаренгейти за допомогою формули та вивести результат в консоль.
+
+// Створити змінну daysInMonth для зберігання кількості днів у місяці. Обчислити кількість hoursInMonth годин та minutesInMonth хвилин у цьому місяці за допомогою оператора множення та вивести результат в консоль.
+var daysInMonth = 31;
+var hoursInMonth = daysInMonth * 24;
+console.log(hoursInMonth, "hours");
+
+var minutesInMonth = daysInMonth * 24 * 60;
+console.log(minutesInMonth, "minutes");
+
+// Створити змінні health, energy для зберігання рівня здоров'я та енергії гравця в грі. Зменшити рівень здоров'я та енергії гравця за допомогою операторів віднімання та вивести результат в консоль.
+
+var health = 100;
+var energy = 100;
+
+console.log(health - 30);
+console.log(energy - 50);
+
+// Створити змінну totalPrice для зберігання суми покупки в магазині. Застосувати знижку discount в розмірі 10% до цієї суми за допомогою оператора множення. Результат зберегти в змінній discountedPrice та вивести результат в консоль.
+
+var totalPrice = 130;
+var discount = 0.10;
+var discountedPrice = totalPrice * (1 - discount);
+
+console.log(discountedPrice);
+
+
